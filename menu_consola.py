f0f5c15992a27f5655a78aa1215c26fe871c1820
Ariel <ariel.ortega.zitle98@gmail.com>
2021-11-01 19:31:55 -0600

    Se agrego el tercer juego
    Se modifico el menú principal para agregar el tercer botón y se agrego el archivo del tercer juego

--- menu_consola.py
+++ menu_consola.py
@@ -6,7 +6,7 @@ import subprocess
 mainClock = pygame.time.Clock()
 from pygame.locals import *
 pygame.init()
-pygame.display.set_caption("Learn and get fun")
+pygame.display.set_caption("Consola de videojuegos")
 icon = pygame.image.load('icono.png')
 pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((800, 600),0,32)
@@ -30,7 +30,7 @@ def main_menu():
         screen.fill((0,0,0))
         #poner fondo constantemente
         screen.blit(background, (0,0))
-        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+        draw_text('Consola de videojuegos!! Diviertete con estos tres minijuegos', font, (255, 64, 17), screen, 200, 20)
  
         mx, my = pygame.mouse.get_pos()
  
@@ -38,16 +38,24 @@ def main_menu():
         #pygame.Rect(margen izquierda, margen top, largo, alto)
         #draw_text('Juego 1', font, (255, 255, 255), screen, 60, 90)
         button_2 = pygame.Rect(350, 200, 100, 50)
+
+        button_3 = pygame.Rect(350, 300, 100, 50)
+
         if button_1.collidepoint((mx, my)):
             if click:
                 game() #mandamos a llamar al metodo del juego.
         if button_2.collidepoint((mx, my)):
             if click:
                 game2()
+        if button_3.collidepoint((mx, my)):
+            if click:
+                game3()
         pygame.draw.rect(screen, (255, 0, 0), button_1)
-        draw_text('Juego 1', font, (255, 255, 255), screen, 360, 120)
+        draw_text('Memorama', font, (255, 255, 255), screen, 360, 120)
         pygame.draw.rect(screen, (255, 0, 0), button_2)
-        draw_text('Juego 2', font, (255, 255, 255), screen, 360, 220)
+        draw_text('Hacker-fight', font, (255, 255, 255), screen, 360, 220)
+        pygame.draw.rect(screen, (255, 0, 0), button_3)
+        draw_text('Zudoku', font, (255, 255, 255), screen, 360, 320)
  
         click = False
         for event in pygame.event.get():
@@ -84,21 +92,13 @@ def game():
     subprocess.getoutput('python memorypuzzle.py')
  
 def game2():
-    """running = True
-    while running:
-        screen.fill((0,0,0))
- 
-        draw_text('options', font, (255, 255, 255), screen, 20, 20)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-        
-        pygame.display.update()
-        mainClock.tick(60)"""
     subprocess.getoutput('python hacker.py')
+
+def game3():
+    subprocess.getoutput('python GUI.py')
+
+
+
+
 #se manda a ejecutar menu principal 
 main_menu()
